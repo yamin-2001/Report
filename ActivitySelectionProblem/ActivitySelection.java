6560f118fba6cc5e517347d3fdb3d3a401855aef
MD. ASHABUL YAMIN <yamin200185@gmail.com>
2025-05-18 16:17:36 +0600

    Update ActivitySelection.java

--- ActivitySelectionProblem/ActivitySelection.java
+++ ActivitySelectionProblem/ActivitySelection.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class ActivitySelection {
 
     public static void selectActivities(List<Activity> activities) {
@@ -12,3 +13,20 @@ public class ActivitySelection {
             }
         }
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of activities: ");
+        int n = sc.nextInt();
+
+        List<Activity> activities = new ArrayList<>();
+        System.out.println("Enter start and end times of activities:");
+        for (int i = 0; i < n; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            activities.add(new Activity(start, end));
+        }
+
+        selectActivities(activities);
+    }
+}
