b15c41c0d0e851ec1965aee20a4ecb7d532e3106
MD. ASHABUL YAMIN <yamin200185@gmail.com>
2025-05-18 16:38:31 +0600

    Create MergeSort.cpp

---
+++ SortingAlgorithms/MergeSort.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
+}
+
+int main() {
+    cout << "Enter the Size of the Array: ";
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    cout << endl << "Enter Array Elements: ";
+    for (auto &i : arr) cin >> i;
+
+    mergeSort(arr, 0, n - 1);
+    cout << endl << "Array after Merge Sort: ";
+    for (int num : arr)
+        cout << num << " ";
+    cout << endl;
+}
