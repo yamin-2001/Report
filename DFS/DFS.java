00407cfc4206fa2cc2fd9aa4eaa7ad3006ef9593
MD. ASHABUL YAMIN <yamin200185@gmail.com>
2025-05-18 16:30:37 +0600

    Create DFS.java

---
+++ DFS/DFS.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class DFS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of vertices: ");
+        int v = sc.nextInt();
+        System.out.print("Enter number of edges: ");
+        int e = sc.nextInt();
+
+        Graph graph = new Graph(v);
+
+        System.out.println("Enter edges (u v):");
+        for (int i = 0; i < e; i++) {
+            int u = sc.nextInt();
+            int v1 = sc.nextInt();
+            graph.addEdge(u, v1);
+        }
+
+        System.out.print("Enter starting vertex for DFS: ");
+        int start = sc.nextInt();
+
+        graph.dfs(start);
+    }
+}
