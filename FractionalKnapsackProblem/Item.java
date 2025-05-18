618ce74fc9bc2e33f01dad96c59985661add2432
MD. ASHABUL YAMIN <yamin200185@gmail.com>
2025-05-18 16:20:25 +0600

    Create Item.java

---
+++ FractionalKnapsackProblem/Item.java
@@ -0,0 +1,10 @@
+class Item {
+    int weight, value;
+    double ratio;
+
+    public Item(int value, int weight) {
+        this.value = value;
+        this.weight = weight;
+        this.ratio = (double) value / weight;
+    }
+}
