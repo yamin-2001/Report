79c66b6a8badb245fea30b5af34d24b3da6a75e2
MD. ASHABUL YAMIN <yamin200185@gmail.com>
2025-05-19 07:54:59 +0600

    Update HuffmanComparator.java

--- HuffmanCoding/HuffmanComparator.java
+++ HuffmanCoding/HuffmanComparator.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class HuffmanComparator implements Comparator<HuffmanNode> {
     public int compare(HuffmanNode n1, HuffmanNode n2) {
         return Integer.compare(n1.freq, n2.freq);
